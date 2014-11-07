7ac123f10f6d3ce1b49892902de5f5ae55bfee9b
iambipul <bipulkumarbal@outlook.com>
2014-11-07 23:47:12 +0530

    Fee UI

--- client/templates/product_feeds/product_feed.js
+++
@@ -1,9 +0,0 @@
-
-Template.productFeed.helpers({
-  pagePref: function() {
-    return {
-      locale_name: 'en-gb',
-      region_pref: 2
-    };
-  }
-});
--- client/templates/product_feeds/product_feed_list.js
+++ client/templates/product_feeds/product_feed_list.js
@@ -1,15 +1,20 @@
-Template.productFeedList.helpers({ productFeeds: function() {
+Template.productFeedList.helpers({
+  productFeeds: function() {
     var feeds = [];
-    var i = 0;
     ProductFeeds.find().forEach(function(feed){
-      feed.first_in_a_row = i == 0;
+      feed.users = 'Bipul, Shekhar, Nishith and 10 more people bought this';
+      feed.name = feed.name["en-gb"];
       feeds.push(feed);
-      i+=1;
-      if(i%3 ==0){
-        i = 0
-      }
-
     });
   return feeds;
+  },
+
+  pagePref: function() {
+    return {
+      locale_name: 'en-gb',
+      region_pref: 2
+    };
   }
 });
+
+
