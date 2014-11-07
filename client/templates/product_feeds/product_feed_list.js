9cf30f2792eb770438fc8321264088cadef36f7c
iambipul <bipulkumarbal@outlook.com>
2014-11-07 16:59:26 +0530

    product feed list UI

--- client/templates/product_feeds/product_feed_list.js
+++ client/templates/product_feeds/product_feed_list.js
@@ -1,4 +1,15 @@
-Template.productFeedList.helpers({ posts: function() {
-  return ProductFeeds.find();
-}
+Template.productFeedList.helpers({ productFeeds: function() {
+    var feeds = [];
+    var i = 0;
+    ProductFeeds.find().forEach(function(feed){
+      feed.first_in_a_row = i == 0;
+      feeds.push(feed);
+      i+=1;
+      if(i%3 ==0){
+        i = 0
+      }
+
+    });
+  return feeds;
+  }
 });
